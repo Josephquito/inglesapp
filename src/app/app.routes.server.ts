def3c63cb6020afb852a6d366f98526662b7a648
Josephquito <jdquito.3@sudamericano.edu.ec>
2026-02-26 13:41:46 -0500

    app

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,20 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  // ✅ estáticas: puedes prerender
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'mis-cursos', renderMode: RenderMode.Prerender },
+  { path: 'cursos', renderMode: RenderMode.Prerender },
+  { path: 'usuarios', renderMode: RenderMode.Prerender },
+  { path: 'entidad', renderMode: RenderMode.Prerender },
+  { path: 'cambiar-clave', renderMode: RenderMode.Prerender },
+
+  // ❌ dinámicas (tienen params): NO prerender
+  { path: 'cursos/:id', renderMode: RenderMode.Server },
+  { path: 'curso/:id', renderMode: RenderMode.Server },
+  { path: 'curso/:id_curso/evaluacion/:id_evaluacion', renderMode: RenderMode.Server },
+  { path: 'rendiciones/intentos/:id_intento', renderMode: RenderMode.Server },
+
+  // fallback: server (no prerender)
+  { path: '**', renderMode: RenderMode.Server },
 ];
